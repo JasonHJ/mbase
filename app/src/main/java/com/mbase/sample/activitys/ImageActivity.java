d5e0726bb50f0b32dc25b5cbae45c13c1e524670
Chen Lei <chenlei@kanzhun.com>
2015-12-31 15:50:41 +0800

    代码调整，图片加载测试调整

--- app/src/main/java/com/mbase/sample/activitys/ImageActivity.java
+++ app/src/main/java/com/mbase/sample/activitys/ImageActivity.java
@@ -3,13 +3,15 @@ package com.mbase.sample.activitys;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.mbase.monch.utils.ListUtils;
+import com.mbase.monch.utils.StringUtils;
 import com.mbase.monch.view.MImageView;
 import com.mbase.sample.R;
 
@@ -48,9 +50,13 @@ public class ImageActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_images);
+        setContentView(R.layout.activity_images);String titleText = getIntent().getStringExtra("titleText");
+        if (StringUtils.isNotEmpty(titleText)) {
+            TextView titleView = (TextView) findViewById(R.id.title);
+            titleView.setText(titleText);
+        }
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         recyclerView.setAdapter(new ImageAdapter(this, URL_LIST));
     }
 
